a27707cb0bb50f32fb08cf2a85582ed39a052d48
Marko Marinkovic <marko.marinkovic@sogeti.com>
2022-08-02 22:06:59 +0200

    Log-in now working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ function Login(props) {
 
     return (
         <div class="login">
-        <button class="login">Logga in</button>
+        <button class="login" onclick="location.href='https://icy-cliff-03f1e8d03.1.azurestaticapps.net/.auth/login/aad'" type="button">Logga in</button>
         </div>
     );
 }
