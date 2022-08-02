2df7ba97d6be0845a4b4ff9da0d1c1cf59fd2446
Marko Marinkovic <marko.marinkovic@sogeti.com>
2022-08-02 22:13:56 +0200

    Added fallback syntax

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,7 @@ function App() {
     });
   return (
       <div class={styles.App}>
-          <Show when={userInfo() === ""}>
-          <Login />
-          </Show>
-          <Show when={userInfo() !== "" && userInfo()}>
+          <Show when={userInfo() !== "" && userInfo()} fallback={<Login />}>
         <BookingTable />
           </Show>
       </div>
