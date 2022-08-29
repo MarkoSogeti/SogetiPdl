c2c7fd9f5e2d75995c476457080b567f0700d76b
Marko Marinkovic <marko.marinkovic@sogeti.com>
2022-08-29 14:59:21 +0200

    Changed to swedish date format

--- src/components/TimeSlot.jsx
+++ src/components/TimeSlot.jsx
@@ -5,7 +5,10 @@ import CheckMark from "./CheckMark";
 function TimeSlot(props) {
   return (
     <div>
-      <p>{props.time ? new Date(props.time).toLocaleDateString() : ""} 17:00</p>
+      <p>
+        {props.time ? new Date(props.time).toLocaleDateString("sv-SE") : ""}{" "}
+        17:00
+      </p>
       <p className="remainingPlaces">
         Återstående platser: {4 - props.participants}
       </p>
